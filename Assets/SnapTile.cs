debc6a81712e24486a07e4ce16c21fec67e35f7e
okada <oroshi12340@gmail.com>
2016-03-07 23:45:51 +0900

    エディタオンリーで Sprite スナップ操作

---
+++ Assets/SnapTile.cs
@@ -0,0 +1,26 @@
+﻿// @file SnapTile.cs
+// @author Okada Moeto
+
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapTile : MonoBehaviour
+{
+	public float snapValue = 1.0f;
+	
+	void Update()
+	{
+		if(Application.isPlaying)
+		{
+			return;
+		}
+		
+		float x, y;
+		x = Mathf.Round(transform.position.x / snapValue) * snapValue;
+		y = Mathf.Round(transform.position.y / snapValue) * snapValue;
+		
+		transform.position = new Vector3(x, y, 0.0f);
+		
+		GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(-y);
+	}
+}
